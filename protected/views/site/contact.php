32cf2d335d73f1e834d3d4065b01253bcf748575
Cruelkid <akosovich@narod.ru>
2017-09-13 19:23:13 +0300

    added validation

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -61,7 +61,7 @@ If you have business inquiries or other questions, please fill out the following
         <?php echo $form->error($model, 'body'); ?>
     </div>
 
-    <?php if(CCaptcha::checkRequirements()): ?>
+    <?php if (CCaptcha::checkRequirements()) : ?>
     <div class="row">
         <?php echo $form->labelEx($model, 'verifyCode'); ?>
         <div>
