d7200dd4b49645514490fc38c85d691f5b9e6fdc
Cruelkid <akosovich@narod.ru>
2017-09-13 19:20:35 +0300

    added validation

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -11,13 +11,13 @@ $this->breadcrumbs=array(
 
 <h1>Contact Us</h1>
 
-<?php if(Yii::app()->user->hasFlash('contact')): ?>
+<?php if (Yii::app()->user->hasFlash('contact')) : ?>
 
 <div class="flash-success">
     <?php echo Yii::app()->user->getFlash('contact'); ?>
 </div>
 
-<?php else: ?>
+<?php else : ?>
 
 <p>
 If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
@@ -29,7 +29,7 @@ If you have business inquiries or other questions, please fill out the following
     'id'=>'contact-form',
     'enableClientValidation'=>true,
     'clientOptions'=>array(
-    	'validateOnSubmit'=>true,
+        'validateOnSubmit'=>true,
     ),
 )); ?>
 
@@ -38,39 +38,39 @@ If you have business inquiries or other questions, please fill out the following
     <?php echo $form->errorSummary($model); ?>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'name'); ?>
-        <?php echo $form->textField($model,'name'); ?>
-        <?php echo $form->error($model,'name'); ?>
+        <?php echo $form->labelEx($model, 'name'); ?>
+        <?php echo $form->textField($model, 'name'); ?>
+        <?php echo $form->error($model, 'name'); ?>
     </div>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'email'); ?>
-        <?php echo $form->textField($model,'email'); ?>
-        <?php echo $form->error($model,'email'); ?>
+        <?php echo $form->labelEx($model, 'email'); ?>
+        <?php echo $form->textField($model, 'email'); ?>
+        <?php echo $form->error($model, 'email'); ?>
     </div>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'subject'); ?>
-        <?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-        <?php echo $form->error($model,'subject'); ?>
+        <?php echo $form->labelEx($model, 'subject'); ?>
+        <?php echo $form->textField($model, 'subject', array('size'=>60, 'maxlength'=>128)); ?>
+        <?php echo $form->error($model, 'subject'); ?>
     </div>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'body'); ?>
-        <?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
-        <?php echo $form->error($model,'body'); ?>
+        <?php echo $form->labelEx($model, 'body'); ?>
+        <?php echo $form->textArea($model, 'body', array('rows'=>6, 'cols'=>50)); ?>
+        <?php echo $form->error($model, 'body'); ?>
     </div>
 
     <?php if(CCaptcha::checkRequirements()): ?>
     <div class="row">
-        <?php echo $form->labelEx($model,'verifyCode'); ?>
+        <?php echo $form->labelEx($model, 'verifyCode'); ?>
         <div>
         <?php $this->widget('CCaptcha'); ?>
-        <?php echo $form->textField($model,'verifyCode'); ?>
+        <?php echo $form->textField($model, 'verifyCode'); ?>
         </div>
         <div class="hint">Please enter the letters as they are shown in the image above.
         <br/>Letters are not case-sensitive.</div>
-        <?php echo $form->error($model,'verifyCode'); ?>
+        <?php echo $form->error($model, 'verifyCode'); ?>
     </div>
     <?php endif; ?>
 
